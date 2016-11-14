76be6631d6eae55136e9e458bad05f743d132118
laazer <jabrandt31@gmail.com>
2016-11-13 23:21:18 -0500

    fixed compound function problem

--- app/src/main/java/com/laazer/swogger/models/ExerSuperSet.java
+++ app/src/main/java/com/laazer/swogger/models/ExerSuperSet.java
@@ -3,6 +3,7 @@ package com.laazer.swogger.models;
 import java.util.ArrayList;
 import java.util.List;
 import com.laazer.common.collections.CollectionUtils;
+import com.laazer.common.collections.ListUtils;
 import com.laazer.common.functions.Function;
 import com.laazer.common.functions.Functions;
 
@@ -20,7 +21,15 @@ public class ExerSuperSet implements ExerSet {
 
     @Override
     public List<String> getTags() {
-        return CollectionUtils.fold(new ArrayList<String>(), Functions.toBinFunction(new AppendTags()),
+        Function<ExerSet, List<String>> toStringList = new Functions.AFunction<ExerSet, List<String>>() {
+            @Override
+            public List<String> apply(ExerSet exerSet) {
+                return exerSet.getTags();
+            }
+        };
+        Function<List<String>, Function<ExerSet, List<String>>> func = Functions.compound(ListUtils.<String>append(), toStringList);
+        return CollectionUtils.fold(new ArrayList<String>(),
+                Functions.toBinFunction(func),
                 this.sets);
     }
 
--- app/src/main/java/com/laazer/swogger/models/Schedule.java
+++ app/src/main/java/com/laazer/swogger/models/Schedule.java
@@ -1,7 +1,7 @@
 package com.laazer.swogger.models;
 
 /**
- * Created by Dexter on 11/9/2016.
+ * Created by Laazer
  */
 public class Schedule {
 }
